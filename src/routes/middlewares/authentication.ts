459b109a6857b47cf694cffb6f431436403ae78c
alexanderbry <alexunderbrain@gmail.com>
2025-02-24 00:49:41 +0700

    debugging while deploy

--- src/routes/middlewares/authentication.ts
+++ src/routes/middlewares/authentication.ts
@@ -9,15 +9,19 @@ const authentication = async (
 ) => {
   try {
     if (!req.headers.authorization) throw { name: "Unauthorized" };
+console.log(req.headers.authorization, "ini authorization");
 
     const [bearer, token] = req.headers.authorization.split(" ");
     if (!token) throw { name: "Unauthorized" };
+console.log(token, "ini token");
 
     const data = await verifyToken(token);
     if (typeof data === "string" || !data.id) throw { name: "Unauthorized" };
+console.log(data, "ini data");
 
     const user = await findById(data.id);
     if (!user) throw { name: "Unauthorized" };
+console.log(user, "ini user");
 
     res.locals.loginSession = {
       id: user.id,
